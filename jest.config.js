a94ecb712bc5c4d50b8c59270199b918f6a03bcb
olha.kolmakova <olha.kolmakova@xenial.com>
2025-05-22 18:36:00 +0200

    part1

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('jest').Config} */
+export default {
+  testEnvironment: 'node',
+  testTimeout: 30000,
+  verbose: true,
+  testMatch: ['**/*.test.js'],
+}
---
+++ test/e2e.test.js
@@ -0,0 +1,39 @@
+import "chromedriver" 
+import { browser, command, have, be } from "selenidejs" 
+import { Key } from "selenium-webdriver"
+
+let URL = `https://demoqa.com/automation-practice-form` 
+
+describe("Practice Form Test", function () {
+  beforeEach(async () => {
+    browser.config.windowHeight = "1080" 
+    browser.config.windowWidth = "1920" 
+    browser.config.timeout = 2000
+    console.log("Start testing")
+  })
+
+  afterAll(async () => {
+    //await browser.quit()
+    console.log("End testing")
+  })
+
+  it("E2E Practice Form Test)", async () => {
+    await browser.open(`${URL}`)
+    await browser.element("#firstName").setValue("Olha")
+    await browser.element("#lastName").setValue("Kolmakova")
+    await browser.element("#userEmail").setValue("olha@gmail.com")
+    await browser.element('label[for="gender-radio-1"]').click()
+    await browser.element('#userNumber').setValue("77777777")
+    await browser.element("input#dateOfBirthInput").click()
+    await browser.element(".react-datepicker__month-select").click()
+    await browser.element('.react-datepicker__month-select [value="7"]').click()
+    await browser.element('.react-datepicker__year-select').click()
+    await browser.element('.react-datepicker__year-select [value="1989"]').click()
+    await browser.element('.react-datepicker__month').click()
+    await browser.element('.react-datepicker__day--019').click()
+  })
+})
+
+//await browser.executeScript(`document.getElementById('dateOfBirthInput').value = '15 Jul 1995'`)
+ // await browser.element("input#dateOfBirthInput").sendKeys(Key.META,'a')
+// await browser.element("input#dateOfBirthInput").sendKeys(Key.DELETE)
