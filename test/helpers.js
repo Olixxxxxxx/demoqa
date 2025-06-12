072f2cffc06ad44852ddc3421c9aded5623cbb4c
olha.kolmakova <olha.kolmakova@xenial.com>
2025-06-13 01:49:04 +0200

    fix

--- test/helpers.js
+++ test/helpers.js
@@ -1,6 +1,6 @@
 import { browser, have, be } from "selenidejs";
 export class Helpers {
-  static closeBanners() {
+  static closeBanners(){
     browser.executeScript(`
     document.querySelectorAll(
       '.Google-Ad, iframe[id^="google_ads_iframe_"], div[id^="Ad.Plus-"]').forEach(el => el.remove());
@@ -13,5 +13,15 @@ export class Helpers {
     browser
       .element(`table tbody tr:nth-child(${rowNumber}) td:nth-child(2)`)
       .should(have.text(Data))
+  }
+
+  static async setDateOfBirth(day,month,year){
+    await browser.element("input#dateOfBirthInput").click()
+    await browser.element(".react-datepicker__month-select").click()
+    await browser.element(`.react-datepicker__month-select [value="${month-1}"]`).click()
+    await browser.element(".react-datepicker__year-select").click()
+    await browser.element(`.react-datepicker__year-select [value="${year}"]`).click()
+    await browser.element(".react-datepicker__month").click()
+    await browser.element(`.react-datepicker__day--0${day}`).click()
   }
 }
